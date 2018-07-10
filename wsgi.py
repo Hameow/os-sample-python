45aef94bccc610da8faea0d24fe74f73110f0d90
Hisham <hisham.web.developer@gmail.com>
2018-07-10 17:52:35 +0800

    Updated mongodb error msg

--- wsgi.py
+++ wsgi.py
@@ -2,18 +2,18 @@ import pymongo
 from flask import Flask
 application = Flask(__name__)
 
-try:
-    client = pymongo.MongoClient('172.30.104.15',
-                                27017,
-                                username='userA24',
-                                password='uwgDyvIAgUlSjSEJ')
-    db = client['sampledb']
-except:
-    print("Can't connect to MongoDB server with database itwonders.")
-
 @application.route("/")
 def hello():
-    return "Hello World!"
+    try:
+        client = pymongo.MongoClient('172.30.104.15',
+                                    27017,
+                                    username='userA24',
+                                    password='uwgDyvIAgUlSjSEJ')
+        db = client['sampledb']
+        return "Success"
+    except:
+        return "Can't connect to MongoDB server with database itwonders."
+        #return "Hello World!"
 
 if __name__ == "__main__":
     application.run()

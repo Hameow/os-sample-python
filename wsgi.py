50f9a7a5d3d6cbb95ba67ca596247064c90f0d58
Hisham <hisham.web.developer@gmail.com>
2018-07-10 17:41:14 +0800

    pymongo

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,15 @@
+import pymongo
 from flask import Flask
 application = Flask(__name__)
+
+try:
+    client = pymongo.MongoClient('172.30.104.15',
+                                27017,
+                                username='userA24',
+                                password='uwgDyvIAgUlSjSEJ')
+    db = client['sampledb']
+except:
+    print("Can't connect to MongoDB server with database itwonders.")
 
 @application.route("/")
 def hello():
